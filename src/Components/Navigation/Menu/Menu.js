ba73da190cf0b903d7ec5a23999fa0d25ba2b300
malk1405 <a.a.malkin@mai.ru>
2019-05-07 14:58:49 +0300

    Исправил выпадающее меню

--- src/Components/Navigation/Menu/Menu.js
+++ src/Components/Navigation/Menu/Menu.js
@@ -1,40 +1,53 @@
 import React, { Component } from "react";
 import { AuthContext } from "../../../Context/auth";
-import { withRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Menu extends Component {
   state = { isVisible: false };
-  toggleMenu = () => {
+
+  showMenu = () => {
+    this.setState({ isVisible: true });
+  };
+
+  hideMenu = () => {
+    this.setState({ isVisible: false });
+  };
+
+  toggleMenu = e => {
+    e.stopPropagation();
     this.setState(state => {
       return { isVisible: !state.isVisible };
     });
   };
-  toCabinet = id => {
-    this.props.history.push(`/authors/${id}`);
-  };
+
   render() {
     return (
       <AuthContext.Consumer>
         {({ user, logout }) => (
-          <div className="user">
+          <div
+            onMouseEnter={this.showMenu}
+            onMouseLeave={this.hideMenu}
+            onClick={this.hideMenu}
+          >
             <button name="user" onClick={this.toggleMenu}>{`${user.name} ${
               user.lastname
             }`}</button>
             {this.state.isVisible ? (
-              <React.Fragment>
+              <div
+                style={{
+                  position: "absolute",
+                  display: "flex",
+                  flexDirection: "column"
+                }}
+              >
                 <Link to={`/authors/${user._id}`}>Личный кабинет</Link>
-                <button
-                  onClick={() => {
-                    this.toCabinet(user._id);
-                  }}
-                />
                 <button name="user">Добавить статью</button>
                 <button name="user">Мои статьи</button>
                 <Link to="/articles">На главную</Link>
                 <button name="user" onClick={logout}>
                   Выйти
                 </button>
-              </React.Fragment>
+              </div>
             ) : null}
           </div>
         )}
@@ -43,4 +56,4 @@ class Menu extends Component {
   }
 }
 
-export default withRouter(Menu);
+export default Menu;
