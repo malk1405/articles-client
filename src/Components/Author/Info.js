38e95b2714a7a6ed3ac98a8285150f2031605259
malk1405 <a.a.malkin@mai.ru>
2019-05-08 17:08:20 +0300

    Опробовал react hooks

--- src/Components/Author/Info.js
+++ src/Components/Author/Info.js
@@ -1,55 +1,46 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
-export default class Info extends Component {
-  signal = Axios.CancelToken.source();
+const Info = ({ pathname }) => {
+  const [user, setUser] = useState({});
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  state = {
-    user: {},
-    error: false,
-    isLoading: true
-  };
-
-  componentDidMount() {
-    this.onLoadUser();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.pathname !== prevProps.pathname) {
-      this.signal.cancel("Api is being canceled");
-      this.onLoadUser();
-    }
-  }
-
-  componentWillUnmount() {
-    this.signal.cancel("Api is being canceled");
-  }
-
-  onLoadUser = async () => {
-    try {
-      this.setState({ isLoading: true, error: false });
-      const response = await Axios.get(`/api/${this.props.pathname}`, {
-        cancelToken: this.signal.token
-      });
-      this.setState({ user: response.data, isLoading: false });
-    } catch (err) {
-      if (Axios.isCancel(err)) {
-      } else {
-        this.setState({ isLoading: false, error: true });
+  useEffect(() => {
+    const signal = Axios.CancelToken.source();
+    const onLoadUser = async () => {
+      try {
+        setIsLoading(true);
+        setError(false);
+        const response = await Axios.get(`/api/${pathname}`, {
+          cancelToken: signal.token
+        });
+        setIsLoading(false);
+        setUser(response.data);
+      } catch (err) {
+        if (Axios.isCancel(err)) {
+        } else {
+          setIsLoading(false);
+          setError(true);
+        }
       }
-    }
-  };
+    };
+    onLoadUser();
+    return () => {
+      signal.cancel("Api is being canceled");
+    };
+  }, [pathname]);
 
-  render() {
-    if (this.state.isLoading) return <p>Загрузка...</p>;
-    if (this.state.error) return <p>Ошибка</p>;
-    return (
-      <ul>
-        <li>Имя: {this.state.user.name}</li>
-        <li>Фамилия: {this.state.user.lastname}</li>
-        <li>Дата рождения:</li>
-        <li>Email: {this.state.user.email}</li>
-      </ul>
-    );
-  }
-}
+  if (isLoading) return <p>Загрузка...</p>;
+  if (error) return <p>Ошибка</p>;
+  return (
+    <ul>
+      <li>Имя: {user.name}</li>
+      <li>Фамилия: {user.lastname}</li>
+      <li>Дата рождения:</li>
+      <li>Email: {user.email}</li>
+    </ul>
+  );
+};
+
+export default Info;
