14e6172b15e2a718c93f9666f5d703f600bb39d4
malk <malk1405@mail.ru>
2019-05-09 09:58:16 +0300

    Сделал сохранение пользователя в localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import React from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useState,
+  useMemo,
+  useRef
+} from "react";
 import { AuthContext } from "./Context/auth";
 import { Route, Redirect, Switch } from "react-router-dom";
 import Articles from "./Components/Articles/Articles";
@@ -8,39 +14,52 @@ import Navigation from "./Components/Navigation/Navigation";
 
 const RedirectFromHome = () => <Redirect to="/articles" />;
 
-class App extends React.Component {
-  login = user => {
-    this.setState({
-      user: { ...user }
-    });
-  };
+const App = () => {
+  const [user, setUser] = useState(null);
 
-  logout = () => {
-    this.setState({ user: null });
-  };
+  const login = useCallback(user => {
+    setUser({ ...user });
+  }, []);
 
-  state = { user: null, login: this.login, logout: this.logout };
+  const logout = useCallback(() => {
+    setUser(null);
+  }, []);
 
-  render() {
-    return (
-      <AuthContext.Provider value={this.state}>
-        <div className="grid-container">
-          <Navigation />
-          <main>
-            <Switch>
-              {/* <Route path="/articles/:articleId" component={Article} /> */}
-              <Route path="/articles" component={Articles} />
-              <Route path="/authors/:authorId" exact component={Author} />
-              {/* <Route path="/authors" component={Authors} />
+  const justMounted = useRef(true);
+
+  useEffect(() => {
+    if (justMounted.current) {
+      setUser(JSON.parse(window.localStorage.getItem("user")));
+    } else {
+      window.localStorage.setItem("user", JSON.stringify(user));
+    }
+    justMounted.current = false;
+  }, [user]);
+
+  const value = useMemo(
+    () => ({ user, login, logout }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [user]
+  );
+
+  return (
+    <AuthContext.Provider value={value}>
+      <div className="grid-container">
+        <Navigation />
+        <main>
+          <Switch>
+            {/* <Route path="/articles/:articleId" component={Article} /> */}
+            <Route path="/articles" component={Articles} />
+            <Route path="/authors/:authorId" exact component={Author} />
+            {/* <Route path="/authors" component={Authors} />
             <Route path="/new-author" component={NewAuthor} />
             <Route path="/new-article" component={NewAritcle} /> */}
-              <Route path="/" component={RedirectFromHome} />
-            </Switch>
-          </main>
-        </div>
-      </AuthContext.Provider>
-    );
-  }
-}
+            <Route path="/" component={RedirectFromHome} />
+          </Switch>
+        </main>
+      </div>
+    </AuthContext.Provider>
+  );
+};
 
 export default App;
