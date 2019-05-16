882b72fcbbd6fd639371040b4b9d57dee77f25a9
malk1405 <a.a.malkin@mai.ru>
2019-05-16 18:02:05 +0300

    Временная неработающая версия

--- src/Components/Articles/NewArticle.js
+++ src/Components/Articles/NewArticle.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import useForm from "../../hooks/useForm";
 import { AuthContext } from "../../Context/auth";
 import useAxios from "../../hooks/useAxios";
@@ -7,12 +7,12 @@ import "./newArticle.css";
 const url = `/api/authors/?search=`;
 
 const NewArticle = ({ hide }) => {
-  const context = useContext(AuthContext);
+  const { user: currentUser } = useContext(AuthContext);
   const [errorText, setErrorText] = useState("");
-  const [authors, setAuthors] = useState([]);
   const [id, setId] = useState(0);
+  const [authors, setAuthors] = useState([{ id, ...currentUser }]);
   const [resAuthors, setResAuthors] = useState([]);
-
+  const [isAdding, setIsAdding] = useState(false);
   const { setData, setIsFetching } = useAxios({
     url: "/api/articles",
     method: "post",
@@ -20,65 +20,73 @@ const NewArticle = ({ hide }) => {
     onFailure: setErrorText
   });
 
-  const { setUrl, setIsFetching: setisGetting } = useAxios({
-    onSuccess: ({ data }) => {
-      setResAuthors(data);
-    },
-    onFailure: setErrorText
-  });
+  // const { setUrl, setIsFetching: setisGetting } = useAxios({
+  //   onSuccess: ({ data }) => {
+  //     setResAuthors(data);
+  //   },
+  //   onFailure: setErrorText
+  // });
 
   const { values, handleChange, handleSubmit, handleReset } = useForm({
     submit: () => {
-      const { _id: authorId, name, lastname } = context.user;
       setData({
         ...values,
         publicationDate: +values.publicationDate,
         pages: values.pages,
-        authors: [
-          { authorId, name, lastname },
-          ...authors.map(({ value }) => {
-            const [name, lastname] = value.split(" ");
-            return { name, lastname };
-          })
-        ]
+        authors
       });
       setIsFetching(true);
     }
   });
-  const addAuthor = () => {
-    setAuthors([...authors, { id, value: "" }]);
-    setId(id + 1);
+
+  const handleAdd = () => {
+    setIsAdding(true);
   };
 
-  const deleteAuthor = ({ target: { htmlFor } }) => {
-    const result = [...authors];
-    const index = result.findIndex(({ id }) => {
-      return id === +htmlFor;
-    });
-    if (index >= 0) {
-      setAuthors(result.filter((el, i) => i !== index));
-    }
+  const handleDelete = () => {
+    setIsAdding(false);
   };
 
-  const handleChangeAuthors = ({ target: { name, value } }) => {
-    const result = [...authors];
-    const index = result.findIndex(({ id }) => {
-      return id === +name;
-    });
-    setisGetting(false);
-    setUrl(url + value);
-    setisGetting(true);
-    if (index >= 0) {
-      result[index] = { ...result[index], value };
-      setAuthors(result);
-    }
-  };
+  // const deleteAuthor = ({ target: { htmlFor } }) => {
+  //   const result = [...authors];
+  //   const index = result.findIndex(({ id }) => {
+  //     return id === +htmlFor;
+  //   });
+  //   if (index >= 0) {
+  //     setAuthors(result.filter((el, i) => i !== index));
+  //   }
+  // };
+
+  // const handleChangeAuthors = ({ target: { name, value } }) => {
+  //   // const result = [...authors];
+  //   // const index = result.findIndex(({ id }) => {
+  //   //   return id === +name;
+  //   // });
+  //   // setisGetting(false);
+  //   // setUrl(url + value);
+  //   // setisGetting(true);
+  //   // if (index >= 0) {
+  //   //   result[index] = { ...result[index], value };
+  //   //   setAuthors(result);
+  //   // }
+  // };
+
+  // useEffect(() => {
+  //   if (isVisible) {
+  //   }
+
+  //   return () => {
+  //     setisGetting(false);
+  //   };
+  // }, [isVisible, authors]);
+
   return (
     <div className="modal">
       <form
         className="signup-form"
         onSubmit={handleSubmit}
         onReset={handleReset}
+        autoComplete="off"
       >
         <label>Название: </label>
         <input
@@ -104,35 +112,52 @@ const NewArticle = ({ hide }) => {
           value={values.pages || ""}
           onChange={handleChange}
         />
-        {authors.map((el, index) => {
+        {authors.map(({ id, name, lastname }, index) => {
           return (
-            <React.Fragment key={el.id}>
-              <label htmlFor={el.id} onClick={deleteAuthor}>
-                Соавтор {index + 1}
-              </label>
-              <input
-                list={el.id}
-                name={el.id}
-                value={el.value}
-                onChange={handleChangeAuthors}
-              />
-              <datalist id={el.id}>
-                {resAuthors.map(({ _id, name, lastname }) => {
-                  return (
-                    <option key={_id}>
-                      {name} {lastname}
-                    </option>
-                  );
-                })}
-              </datalist>
+            <React.Fragment key={id}>
+              <p> Автор {index + 1} </p>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <p>
+                  {name} {lastname}{" "}
+                </p>
+                <button type="button" onClick={handleDelete}>
+                  -
+                </button>
+              </div>
             </React.Fragment>
           );
         })}
-        <button type="button" onClick={addAuthor}>
-          Добавить соавтора
+        {isAdding ? (
+          <>
+            <p>Автор {authors.length + 1}</p>
+            <div
+              style={{
+                gridColumn: "2/-1",
+                display: "grid",
+                width: "100%",
+                gridTemplateColumns: "auto min-content min-content"
+              }}
+            >
+              <input style={{}} />
+              <button type="button" onClick={handleDelete}>
+                +
+              </button>
+              <button type="button" onClick={handleDelete}>
+                -
+              </button>
+            </div>
+          </>
+        ) : (
+          <button className="form_button" type="button" onClick={handleAdd}>
+            Добавить автора
+          </button>
+        )}
+        <button className="form_button" type="submit">
+          Сохранить
         </button>
-        <button type="submit">Сохранить</button>
-        <button type="reset">Отменить</button>
+        <button className="form_button" type="reset">
+          Отменить
+        </button>
       </form>
       {errorText !== "" ? <p>{errorText}</p> : null}
     </div>
