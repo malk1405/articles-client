c6a7bed1b10cf56456ecb173d7e647fb86736402
malk1405 <a.a.malkin@mai.ru>
2019-05-17 17:56:43 +0300

    Распарсили Query параметры

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,30 +1,47 @@
 import React, { useState, useEffect } from "react";
 import useAxios from "../../hooks/useAxios";
 
-const Search = ({ location: { search } }) => {
+const Search = ({ location: { search }, history }) => {
   const [data, setData] = useState({});
   const onSuccess = ({ data }) => {
     setData(data);
   };
 
   const { setUrl, setIsFetching } = useAxios({ onSuccess });
-  useEffect(() => {
-    const url = "/api/search/" + search;
-    setIsFetching(true);
-    setUrl(url);
-    return () => {
-      setIsFetching(false);
-    };
-  }, [search]);
+  useEffect(
+    () => {
+      const url = "/api/search/" + search;
+      setIsFetching(true);
+      setUrl(url);
+      return () => {
+        setIsFetching(false);
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [search]
+  );
+
+  const handlePush = ({ target: { name } }) => {
+    let searchArray = search
+      .split(/\?|&/)
+      .filter(el => el)
+      .map(el => el.split(/=/))
+      .filter(([name]) => name !== "page");
+    searchArray.push(["page", name]);
+
+    let query = "?" + searchArray.map(el => el.join("=")).join("&");
+    history.push(query);
+  };
+
   return (
     <>
-      <button>
+      <button name="articles" onClick={handlePush}>
         Статьи
         {typeof data.articlesNumber === "number"
           ? ` ${data.articlesNumber}`
           : null}
       </button>
-      <button>
+      <button name="authors" onClick={handlePush}>
         Авторы{" "}
         {typeof data.authorsNumber === "number"
           ? ` ${data.authorsNumber}`
