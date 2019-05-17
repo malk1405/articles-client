461ffbc7c5678ff53e3419577effdf313d89008a
malk1405 <a.a.malkin@mai.ru>
2019-05-17 17:56:21 +0300

    Удалили ненужный импорт

--- src/Components/Articles/NewArticle.js
+++ src/Components/Articles/NewArticle.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext, useEffect } from "react";
-import useForm from "../../hooks/useForm";
 import { AuthContext } from "../../Context/auth";
 import useAxios from "../../hooks/useAxios";
 import "./newArticle.css";
