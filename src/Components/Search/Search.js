985cca5c067bbe301a751b9a381c159fec487f63
malk1405 <a.a.malkin@mai.ru>
2019-05-18 11:19:56 +0300

    Добавили подсветку активной ссылки

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,5 +1,21 @@
 import React, { useState, useEffect } from "react";
 import useAxios from "../../hooks/useAxios";
+import { getTokens, createQuery } from "../../utils/query";
+
+const buttons = [
+  { name: "articles", title: "Статьи" },
+  { name: "authors", title: "Авторы" }
+];
+const getPageName = tokens => {
+  const index = tokens.findIndex(([name]) => name === "page");
+
+  if (index < 0) return buttons[0].name;
+
+  const value = tokens[index][1];
+
+  if (!buttons.find(({ name }) => name === value)) return buttons[0].name;
+  return value;
+};
 
 const Search = ({ location: { search }, history }) => {
   const [data, setData] = useState({});
@@ -22,31 +38,30 @@ const Search = ({ location: { search }, history }) => {
   );
 
   const handlePush = ({ target: { name } }) => {
-    let searchArray = search
-      .split(/\?|&/)
-      .filter(el => el)
-      .map(el => el.split(/=/))
-      .filter(([name]) => name !== "page");
-    searchArray.push(["page", name]);
+    let tokens = getTokens(search).filter(([name]) => name !== "page");
+    tokens.push(["page", name]);
 
-    let query = "?" + searchArray.map(el => el.join("=")).join("&");
-    history.push(query);
+    history.push(createQuery(tokens));
   };
 
+  const pageName = getPageName(getTokens(search));
+  console.log(pageName);
   return (
     <>
-      <button name="articles" onClick={handlePush}>
-        Статьи
-        {typeof data.articlesNumber === "number"
-          ? ` ${data.articlesNumber}`
-          : null}
-      </button>
-      <button name="authors" onClick={handlePush}>
-        Авторы{" "}
-        {typeof data.authorsNumber === "number"
-          ? ` ${data.authorsNumber}`
-          : null}
-      </button>
+      {buttons.map(({ name, title }) => {
+        const num = data[`${name}Number`];
+        return (
+          <button
+            key={name}
+            name={name}
+            style={{ backgroundColor: pageName === name ? "green" : "grey" }}
+            onClick={handlePush}
+          >
+            {title}
+            {typeof num === "number" ? ` ${num}` : null}
+          </button>
+        );
+      })}
     </>
   );
 };
