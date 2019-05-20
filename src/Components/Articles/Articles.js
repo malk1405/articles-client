84979d6f264d5d57137e306e7360141a39f779b7
malk1405 <a.a.malkin@mai.ru>
2019-05-20 12:58:02 +0300

    Поправили стили статей

--- src/Components/Articles/Articles.js
+++ src/Components/Articles/Articles.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../Context/auth";
+import "./articles.css";
 
 const Articles = ({ articles }) => {
   const { user } = useContext(AuthContext);
@@ -13,8 +14,8 @@ const Articles = ({ articles }) => {
   };
 
   return (
-    <ol>
-      {articles.map(el => {
+    <ol className="articles">
+      {articles.map((el, index) => {
         const authors = () => {
           if (!el.authors.length) return null;
           const result = [];
@@ -22,14 +23,25 @@ const Articles = ({ articles }) => {
             let author = `${el.lastname} ${el.name}`;
             if (el.authorId)
               author = <Link to={`/authors/${el.authorId}`}>{author}</Link>;
-            author = <li key={el._id}>{author}</li>;
+            author = (
+              <li className="articles_author" key={el._id}>
+                {author}
+              </li>
+            );
             result.push(author);
           });
           return <ul>{result}</ul>;
         };
         return (
-          <li key={el._id}>
+          <li
+            className="article"
+            onClick={() => {
+              console.log("VITALINA");
+            }}
+            key={el._id}
+          >
             <div>
+              <span>{index + 1}. </span>
               <span style={{ fontWeight: isMyArticle(el) ? "bold" : "normal" }}>
                 {el.title}
               </span>
